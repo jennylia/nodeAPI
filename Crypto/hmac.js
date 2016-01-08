b05cc786cd6730b9cda2f5b318fd9cf310d3eba5
Jenny <jenny@fingerfoodstudios.com>
2016-01-07 19:17:52 -0800

    just trying to modify it with our test

--- Crypto/hmac.js
+++ Crypto/hmac.js
@@ -1,13 +1,13 @@
 var crypto = require('crypto');
 var hmac;
 var algorithm = 'sha256';
-var key = '0dffot35ZWPRmuW0OuOxeB2dyfVH4EJEmzaLAhTQI';
-var text = 'I love brussel sprouts';
+var key = 'testkeywillnotworkforproduction';
+var text = {id: 42};
 var hash;
 
 hmac = crypto.createHmac(algorithm, key);
 hmac.setEncoding('hex');
-hmac.write(text);
+hmac.write(JSON.stringify(text));
 hmac.end();
 hash = hmac.read();
 
